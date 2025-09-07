c33c996c107b9a8deede089e43abc80ec399669b
AyenitajuSamuel <samuelayenitaju@gmail.com>
2025-09-07 16:16:41 -0700

    updated the ensemble

---
+++ ensemble.py
@@ -0,0 +1,30 @@
+# ensemble_model.py
+from sklearn.ensemble import VotingClassifier, StackingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score
+from dataset import dataset
+
+# Import trained models from your existing scripts
+from rf_model import rf, X_train, X_test, y_train, y_test
+from ann_model import ann_pipeline
+from svm_model import svm_grid
+
+
+voting = VotingClassifier(
+    estimators=[
+        ("rf", rf),
+        ("svm", svm_grid.best_estimator_),
+        ("ann", ann_pipeline)
+    ],
+    voting="soft"
+)
+
+voting.fit(X_train, y_train)
+
+y_pred = voting.predict(X_test)
+y_prob = voting.predict_proba(X_test)[:, 1]
+
+print("\nSoft Voting Ensemble")
+print("ROC-AUC:", roc_auc_score(y_test, y_prob))
+print(classification_report(y_test, y_pred))
+
